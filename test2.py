9fb620da8ce1bcb281e849522d5628c278274fb1
DESKTOP-P2PJIQU\sa <garabeed@gmail.com>
2022-05-23 17:00:37 +0300

    change path file

--- test2.py
+++ test2.py
@@ -7,27 +7,30 @@ import shutil
 
 
     
+path_file_remove = 'C:/Users/sa/Desktop/run auto/model/*.sav'
+source_file_move='C:/Users/sa/Desktop/run auto/' 
+destination_file='C:/Users/sa/Desktop/run auto/model/'
 
 
 def job():
+
     #generate  new file in main folder path
     list1 = ['a', 'b', 'c']
-
     for i in range(len(list1)):
        open('file%s.sav' % i, 'w').write(list1[i])
     print('added')
 
 
     #remove old files in model folder
-    files_remove = glob.glob('C:/Users/sa/Desktop/run auto/model/*.sav')
+    files_remove = glob.glob(path_file_remove)
     for f in files_remove:
         os.remove(f)
     print('Removed')
 
 
     # move files from main directory to model folder
-    source_f = 'C:/Users/sa/Desktop/run auto/' 
-    destination_f = 'C:/Users/sa/Desktop/run auto/model/'
+    source_f = source_file_move
+    destination_f = destination_file
     pattern = "\*.sav"
 
     file_moved = glob.glob(source_f + pattern)
@@ -37,19 +40,12 @@ def job():
     print('Moved')
     print("I'm working...")
 
+job()
 
 
-######run code every day automatically
-######run code every minutes automatically
+# schedule.every(1).minutes.do(job)
+# # schedule.every().day.at("17:13").do(job)
 
-
-schedule.every(1).minutes.do(job)
-# schedule.every().hour.do(job)
-# schedule.every().day.at("17:13").do(job)
-# schedule.every().monday.do(job)
-# schedule.every().wednesday.at("13:15").do(job)
-# schedule.every().minute.at(":17").do(job)
-
-while True:
-    schedule.run_pending()
-    time.sleep(5)
+# while True:
+#     schedule.run_pending()
+#     time.sleep(5)
